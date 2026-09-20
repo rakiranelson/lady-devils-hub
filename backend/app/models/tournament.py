618a6f84d9a3852704cfed16baa25aebe2a7522d
Ra'Kira Nelson <rakira.nelson@duke.edu>
2026-09-20 18:28:38 -0400

    updated orm for tournament

--- backend/app/models/tournament.py
+++ backend/app/models/tournament.py
@@ -12,5 +12,7 @@ class Tournament(Base):
     id: Mapped[int] = mapped_column(ForeignKey("events.id"))
     player_fee: Mapped[Decimal] = mapped_column(Numeric(10, 2))
     team_fee: Mapped[Decimal] = mapped_column(Numeric(10, 2))
+    group_transportation_required: Mapped[bool]
+    lodging_required: Mapped[bool]
 
     event = relationship("Event")
