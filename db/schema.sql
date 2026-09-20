d706cc3b3cf8681dab6b1314193f765dfc5a80d6
Ra'Kira Nelson <rakira.nelson@duke.edu>
2026-09-20 18:25:31 -0400

    added type to the new attributes

--- db/schema.sql
+++ db/schema.sql
@@ -168,8 +168,8 @@ CREATE TABLE tournaments (
     player_fee DECIMAL(12, 2),
     team_fee DECIMAL(12, 2),
     tournament_type VARCHAR(50),
-    group_transportation_required DEFAULT FALSE;
-    lodging_required DEFAULT FALSE;
+    group_transportation_required BOOLEAN DEFAULT FALSE;
+    lodging_required BOOLEAN DEFAULT FALSE;
     registration_deadline TIMESTAMP WITHOUT TIME ZONE,
 );
 
